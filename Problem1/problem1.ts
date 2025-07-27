a32890c2b92d49ad14db937b15102c518d092c37
Bassant Tarek <bassantarekhefny@gmail.com>
2025-07-27 13:14:38 +0300

    Finished Problem1

--- Problem1/problem1.ts
+++ Problem1/problem1.ts
@@ -7,18 +7,14 @@ You can return the answer in any order.
  */
 
 
-let twoSum = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
+ function twoSum(nums:number[], target:number):number[]{
+    for(let i=0; i < nums.length; i++) {
+        for (let j =i+1; j <  nums.length; j++) {
+            if(nums[i] + nums[j] === target){
+                return [i,j];
             }
-
         }
     }
-}
+ }
 
-twoSum([2, 7, 11, 15], 9);
-twoSum([3, 2, 4], 6);
-
-
+ console.log(twoSum([2,7,11,15],9));
