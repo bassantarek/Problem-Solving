6f1f2063c5b9f99b11f5d21d3f0a0e87dcc1ad47
Bassant Tarek <bassantarekhefny@gmail.com>
2025-07-28 08:31:26 +0300

    Creating Problem#2 Folder

---
+++ Problem2/problem2.ts
@@ -0,0 +1,35 @@
+/* 
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ */
+
+
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
