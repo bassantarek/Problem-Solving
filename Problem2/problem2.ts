a4bc1204ed8457bb711eff01956911c172ce8f11
Bassant Tarek <bassantarekhefny@gmail.com>
2025-08-03 12:52:12 +0300

    Fixed connection

--- Problem2/problem2.ts
+++ Problem2/problem2.ts
@@ -1,24 +1,30 @@
 /* 
-Write a function to find the longest common prefix string amongst an array of strings.
-
-If there is no common prefix, return an empty string "".
+Given an integer x, return true if x is a palindrome, and false otherwise.
 
  
 
 Example 1:
 
-Input: strs = ["flower","flow","flight"]
-Output: "fl"
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
 Example 2:
 
-Input: strs = ["dog","racecar","car"]
-Output: ""
-Explanation: There is no common prefix among the input strings.
- 
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
 
-Constraints:
-
-1 <= strs.length <= 200
-0 <= strs[i].length <= 200
-strs[i] consists of only lowercase English letters if it is non-empty.
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
  */
+
+
+function isPalindrome(num: number): boolean {
+    let isReversedNum = parseInt(num.toString().split('').reverse().join(''));
+    return isReversedNum === num ? true : false;
+}
+
+console.log('isReversedNum',isPalindrome(121));
+console.log('isReversedNum',isPalindrome(1222));
